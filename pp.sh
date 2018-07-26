07f55491cfdb5ae828299c8989f037d261c98371
chrinimue <mail99@posteo.me>
2018-07-26 13:32:08 +0200

    Add basic pp.sh file

---
+++ pp.sh
@@ -0,0 +1,6 @@
+cd ~/Downloads/matchless
+# TODO: Check WIFI/LAN conection
+git pull origin master
+echo "Lade Präsentation von GitHub herunter"
+# TODO: Connect with Dropbox (automatic sync)
+# TODO: Check if USB-Drive with the File "currend.odp" is in the USB-Slot (if true, don't download files via internet)
